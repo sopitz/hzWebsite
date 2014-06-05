822676c8706fce50355b0857fa23320cd132083d
Raphael <raphael@pixelitos.de>
2014-06-05 12:57:39 +0200

    initial commit

---
+++ encryption/key.php
@@ -0,0 +1,13 @@
+<?php 
+class Key {
+
+	private $encKey = "tpW+jzBHDfvSY25doAAR20WKC5tQhXLXnxjS3oBS3fJIhZ5G3d+2GvnnhgXfuVKv73/eNNQwphTWSvGQSkYYGA==";
+
+	public function __construct() {
+		
+	}
+	
+	public function getKey() {
+		return $this->encKey;
+	}
+}
